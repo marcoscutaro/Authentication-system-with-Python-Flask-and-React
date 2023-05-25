2493738c4b3d92cdfb7e54902a10e9ce5ef26a84
Marco S <66038200+marcoscutaro@users.noreply.github.com>
2023-05-25 21:50:09 +0200

    Update models.py

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=True)
+    avatar = db.Column(db.String(50), unique=False, nullable=True)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(50), unique=False, nullable=True)
+    surname = db.Column(db.String(50), unique=False, nullable=True)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "avatar": self.avatar,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "surname": self.surname,
+      
         }
+
+
+
